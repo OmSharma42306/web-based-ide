17aed473ecccbaa623808369c84d9380728d862f
Om Sharma <omsharma.83173@gmail.com>
2024-12-07 15:44:27 +0530

    Implemented websockets for all user server events

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const app = express();
+import http from "http"
 
-app.get('/',(req,res)=>{
-    return res.json({msg:"hi"});
+import WebSocket,{ WebSocketServer } from 'ws';
+const PORT = 3000;
+
+const server = http.createServer(function(req,res){
+    return res.json({msg:"Hi"})
 })
 
+const wss = new WebSocketServer({server});
 
-app.listen(3000,()=>{
-    console.log("Server Started! on PORT : 3000")
+wss.on('connection',function connection(ws){
+    ws.on('error',console.error);
+    console.log("User Connected!",ws)
+
+    ws.on('message',function message(data,isBinary){
+        wss.clients.forEach((client)=>{
+            if(client.readyState == WebSocket.OPEN){
+                client.send(data,{binary:isBinary})
+            }
+        })
+    })
+    
 })
+
+server.listen(PORT,()=>{
+    console.log(`Server is Listening on PORT : ${PORT}`);
+});
